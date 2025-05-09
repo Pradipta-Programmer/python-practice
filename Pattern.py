420257981b0b3d7be1e70a0595dae457e161149f
Pradipta Singha <pradiptasingha2006@gmail.com>
2025-05-09 19:26:46 +0530

    Printing a specific pattern

---
+++ Pattern.py
@@ -0,0 +1,5 @@
+n= int(input("Enter the rows: "))
+print("***")
+for i in range(0, n-2):
+    print("* *")
+print("***")
--- Program6.py
+++
@@ -1,5 +0,0 @@
-n= int(input("Enter the rows: "))
-print("***")
-for i in range(0, n-2):
-    print("* *")
-print("***")
